e53816ba5fca93eb66902c6e7ab6a6901b06bb89
Sukhjit <Sukhjit310192@gmail.com>
2020-01-21 04:31:23 -0500

    application properties

---
+++ src/main/java/com/sukhjit/assignment/controller/RestErrorController.java
@@ -0,0 +1,34 @@
+package com.sukhjit.assignment.controller;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@RestController
+public class RestErrorController implements ErrorController {
+
+    private static final String PATH = "/error";
+
+
+    @RequestMapping(value=PATH,method= RequestMethod.GET)
+    public ResponseEntity errorGet(HttpServletRequest request, HttpServletResponse response){
+        HttpHeaders responseHeaders = new HttpHeaders();
+        responseHeaders.set("Page Not Found", "URL not found");
+        return new ResponseEntity("404 Page Not Found", responseHeaders, HttpStatus.NOT_FOUND);
+    }
+
+    @RequestMapping(value=PATH,method= RequestMethod.POST)
+        public ResponseEntity errorPost(HttpServletRequest request, HttpServletResponse response){
+            return new ResponseEntity("Invalid URL. Failed to add new Product.", HttpStatus.NOT_FOUND);
+        }
+
+    public String getErrorPath() {
+        return PATH;
+    }
+}
